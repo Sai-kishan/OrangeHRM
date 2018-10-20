c51640f799a1fbceb108c344a92e3b02eaf93e06
KuruvaV <KuruvaV@HYD-KURUVAV-L.ds.ad.adp.com>
2018-10-20 10:08:23 +0530

    Add Basic Maven Project structure for OrangeHRM

---
+++ OrangeHRM/src/test/java/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class LoginPage 
+{
+	public void display()
+	{
+		System.out.println("Inside Display Method");
+	}
+}
---
+++ OrangeHRM/src/test/java/tests/LoginTest.java
@@ -0,0 +1,9 @@
+package tests;
+
+public class LoginTest 
+{
+	public void displayTest()
+	{
+		System.out.println("Inside Display Test");
+	}
+}
